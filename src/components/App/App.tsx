41d423339e5f804f5d2b49c162067a68bc0e207f
Max Bemax <7510332@gmail.com>
2025-07-05 18:32:26 +0300

    structure of widget

---
+++ src/components/App/App.tsx
@@ -0,0 +1,48 @@
+import type Votes from '../../types/votes';
+import type { VoteType } from '../../types/votes';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import css from './App.module.css';
+import { useState } from 'react';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const handleVote = (option: VoteType) => {
+    setVotes(prev => ({ ...prev, [option]: prev[option] + 1 }));
+  };
+
+  const resetVotes = () => {
+    setVotes({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate =
+    totalVotes > 0 ? Math.round((votes.good / totalVotes) * 100) : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes === 0 ? (
+        <Notification />
+      ) : (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      )}
+    </div>
+  );
+}
